abe805f53e767c4913f65e3cf1b0e6c34423818f
skotep <skotep@gmail.com>
2015-09-24 22:11:06 -0500

    finialize solution for inclass9

--- sample/qunit-inclass9-solution.js
+++ sample/qunit-inclass9-solution.js
@@ -21,11 +21,19 @@ function setupHandlers(root) {
 function moveObject(obj, dt) {
 	// check for undefined values
 	// and set them to zero
+	if (!obj.x) obj.x = 0;
+	if (!obj.y) obj.y = 0;
+	if (!obj.vx) obj.vx = 0;
+	if (!obj.vy) obj.vy = 0;
+	if (!obj.ax) obj.ax = 0;
+	if (!obj.ay) obj.ay = 0;
 
 	// include the acceleration term
-	obj.x = obj.x + dt * obj.vx
-	obj.y = obj.y + dt * obj.vy
+	obj.x = obj.x + dt * obj.vx + 0.5 * obj.ax * dt * dt
+	obj.y = obj.y + dt * obj.vy + 0.5 * obj.ay * dt * dt
 
 	// update the velocities
+	obj.vx = obj.vx + obj.ax * dt
+	obj.vy = obj.vy + obj.ay * dt
 
 }
