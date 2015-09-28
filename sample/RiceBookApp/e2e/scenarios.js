4641962e1a510330e0c4af735a6e0361bdaf4939
skotep <skotep@gmail.com>
2015-09-27 22:41:15 -0500

    initial add of e2e

---
+++ sample/RiceBookApp/e2e/scenarios.js
@@ -0,0 +1,12 @@
+'use strict'
+
+describe('RiceBookApp', function() {
+
+	if('should have user message', function() {
+		browser.get('index.html')
+		// expect(
+		// 	element.all(by.css('h2')).first().getText()
+		// 	).toMatch('Foo')
+		expect(1).toBe(1)
+	})
+});
