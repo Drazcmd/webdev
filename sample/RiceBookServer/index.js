01e4bdb3e69aa1efbbf2929964673b85204f9b5e
skotep <skotep@gmail.com>
2015-10-21 22:58:38 -0500

    update RiceBookServer to slide

--- sample/RiceBookServer/index.js
+++ sample/RiceBookServer/index.js
@@ -1,7 +1,15 @@
 var express = require('express')
-var app = express()
+var bodyParser = require('body-parser')
 
+var app = express()
+app.use(bodyParser.json())
+app.post('/post', addPost)
 app.get('/', hello)
+
+function addPost(req, res) {
+    console.log('Payload received', req.body)
+    res.send(req.body)
+}
 
 function hello(req, res) {
     res.send('Hello World!')
