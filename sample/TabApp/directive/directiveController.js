58078214fde5c1faab0a51e6d52272c65009ca4c
skotep <skotep@gmail.com>
2016-02-17 23:58:41 -0600

    update directive

--- sample/TabApp/directive/directiveController.js
+++ sample/TabApp/directive/directiveController.js
@@ -16,13 +16,19 @@ function myCustom() {
 		link: function(scope, elem, attrs) {
 			console.log('directive loaded')
 		},
-		replace: true
+		replace: true,
+		scope: {
+			magic: '=magic',
+			initial: '=init'
+		}
 	}
 }
 
 function PageCtrl() {
 	var vm = this;
 	vm.name = 'Page Controller'
+	vm.foo = 'it is the foo'
+	vm.bar = 'beat the bar'
 }
 
 DirectiveCtrl.$inject = ['$scope']
