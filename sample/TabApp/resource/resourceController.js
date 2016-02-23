a1f80d02fe4ebe8b141e2e535f53f88b142d022e
skotep <skotep@gmail.com>
2016-02-22 22:25:57 -0600

    update TabApp

--- sample/TabApp/resource/resourceController.js
+++ sample/TabApp/resource/resourceController.js
@@ -7,18 +7,21 @@ angular.module('tabApp')
 	.controller('ResourceCtrl', ResourceCtrl)
 	;
 
-function apiService($resource, apiURL) {
+function apiService($http, $resource, apiURL) {
+    $http.defaults.withCredentials = true
 	return $resource(apiURL + '/:endpoint/:user', { user: '@user' }, 
 		{
+            login    : { method:'POST', params: {endpoint: 'login'  } },
+            logout   : { method: 'PUT' , params: {endpoint: 'logout'  } },
 			getStatus: { method:'GET', params: {endpoint: 'statuses'} },
-			setStatus: { method:'PUT', params: {endpoint: 'statuses'} },
+			setStatus: { method:'PUT', params: {endpoint: 'status'} },
 			getPosts : { method:'GET', params: {endpoint: 'posts' } }
 		})
 }
 
 function LocationService($resource, apiURL) {
 	return $resource(apiURL + '/locations/:user', { user: '@user' }, {
-		update: { method: 'PUT'}
+         update: { url: apiURL + '/location',  method: 'PUT'}
 	})
 }
 
@@ -26,23 +29,47 @@ ResourceCtrl.$inject = ['$http', '$interval', 'api', 'LocationService']
 function ResourceCtrl($http, $interval, api, LocationService) {
 
 	var vm = this
-	vm.user = 'Scott'
 	vm.posts = []
 	vm.loadPosts = loadPosts
-	vm.removePosts = removePost
+	vm.removePost = removePost
 	vm.toggleLoading = toggleLoading
 	vm.stop = null
+    vm.loggedIn = false
 
 	vm.userStatus = ''
 	vm.saveStatus = saveStatus
 	vm.updateLocation = updateLocation
+    vm.login = login
+    vm.logout = logout
 
 	// treat LocationService as classic REST API
-	vm.location = LocationService.get({ user: vm.user })
+	vm.loc = LocationService.get()
 
 	updateStatus()
 
 	//*********** functions ******************//
+
+    function login() {
+         console.log('logging in', vm.username)
+         
+        api.login({'username':vm.username, 'password':vm.password})
+             .$promise.then(function(result) {
+                   console.log('LogIn', result)
+                   updateStatus()
+                   vm.loggedIn = true
+                   vm.password = ''
+                   vm.loc = LocationService.get()
+             })
+    }
+
+    function logout() {
+         api.logout()
+         vm.userStatus =''
+         vm.newUserStatus =''
+         vm.loggedIn = false
+         vm.loc = { }
+    }
+
 
 	function loadPosts() {
 		vm.posts.length = 0
@@ -54,15 +81,12 @@ function ResourceCtrl($http, $interval, api, LocationService) {
 	}
 
 	function removePost(postId) {
-		var index = -1;
-		var len = vm.posts.length;
-		for (var ii = 0; ii < len; ++ii) {
-			if (vm.posts[ii].id === postId) {
-				index = ii;
-				break;
-			}
-		}
-		vm.posts.splice(index, 1)
+        var index = vm.posts.findIndex(function(post) { 
+             return post.id === postId 
+        }) 
+        if (index >= 0) { 
+             vm.posts.splice(index, 1)  
+        } 
 	}
 
 	function toggleLoading() {
@@ -78,30 +102,32 @@ function ResourceCtrl($http, $interval, api, LocationService) {
 	}
 
 	function updateStatus() {
-		api.getStatus({user:vm.user}).$promise.
-		then(function(result) {
-			vm.userStatus = result.status
-			vm.newUserStatus = result.status			
+		api.getStatus().$promise.then(function(result) {
+			vm.userStatus = result.statuses[0].status
+			vm.newUserStatus = vm.userStatus
+            vm.loggedIn = true
 		})
 	}
 
 	function saveStatus() {
-		api.setStatus({user:vm.user, status:vm.newUserStatus}).$promise.
+		api.setStatus({ status: vm.newUserStatus}).$promise.
 		then(function(result) {
+            console.log(result)
 			vm.userStatus = result.status
-			vm.newUserStatus = result.status
+			vm.newUserStatus = vm.userStatus
 		})
 	}
 
 	function updateLocation(which) {
+        console.log('updateLocation', which)
 		switch(which) {
 			case 'lat':
-				vm.location.lat = vm.location.lat + (Math.random() - 0.5)*5
+				vm.loc.location.lat = vm.loc.location.lat + (Math.random() - 0.5)*5
 				break;
 			case 'lng':
-				vm.location.lng = vm.location.lng + (Math.random() - 0.5)*5
+				vm.loc.location.lng = vm.loc.location.lng + (Math.random() - 0.5)*5
 		}
-		vm.location.$update({user:vm.user})
+		vm.loc.$update({user:vm.user})
 	}
 
 } ////////////////// end FifthCtrl ///////////////
