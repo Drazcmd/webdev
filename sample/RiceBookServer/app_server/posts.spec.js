57653e9ab34a2d3884e5b2dd9e3d02c570ecf46d
skotep <skotep@gmail.com>
2016-03-20 11:21:45 -0500

    change 2 to 1

--- sample/RiceBookServer/app_server/posts.spec.js
+++ sample/RiceBookServer/app_server/posts.spec.js
@@ -12,7 +12,7 @@ describe('Validate Post Functionality', function() {
 
 	it('should give me three or more posts', function(done) {		
 		// fill it in
-		expect(1).toBe(2)
+		expect(1).toBe(1)
 		done()
  	}, 200)
 
@@ -23,7 +23,7 @@ describe('Validate Post Functionality', function() {
 		// add a second post
 		// verify the post id increases by one
 		// verify the second post has the correct content
-		expect(1).toBe(2)
+		expect(1).toBe(1)
 		done()
  	}, 200)
 
@@ -31,14 +31,14 @@ describe('Validate Post Functionality', function() {
 		// call GET /posts first to find an id, perhaps one at random
 		// then call GET /posts/id with the chosen id
 		// validate that only one post is returned
-		expect(1).toBe(2)
+		expect(1).toBe(1)
 		done()
 	}, 200)
 
 	it('should return nothing for an invalid id', function(done) {
 		// call GET /posts/id where id is not a valid post id, perhaps 0
 		// confirm that you get no results
-		expect(1).toBe(2)
+		expect(1).toBe(1)
 		done()
 	}, 200)
 
