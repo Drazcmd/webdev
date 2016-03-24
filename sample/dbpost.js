d6a1307d48fc420ca4c3d514d93631f3b4b2d0e0
skotep <skotep@gmail.com>
2016-03-24 17:33:17 -0500

    update

--- sample/dbpost.js
+++ sample/dbpost.js
@@ -28,14 +28,17 @@ function findByAuthor(author, callback) {
 
 new Post({ id: 1, author: 'sep1', img: null, date: new Date().getTime(), body: 'This is my first post'}).save()
 new Post({ id: 2, author: 'sep1', img: null, date: new Date().getTime(), body: 'This is my second post'}).save()
-new Post({ id: 3, author: 'jmg3', img: null, date: new Date().getTime(), body: "This is Max's post"}).save(function(result) {
-     console.log('done with save', result)
+new Post({ id: 3, author: 'jmg3', img: null, date: new Date().getTime(), body: "This is Max's post"}).save(function() {
+     console.log('done with save')
+     Post.find().exec(function(err, items) { 
+          console.log("There are " + items.length + " posts total in db") 
 
-     findByAuthor('sep1', function() {
-         findByAuthor('jmg3', function() {
-             console.log('complete')
-             process.exit()
-         })
+          findByAuthor('sep1', function() {
+              findByAuthor('jmg3', function() {
+                  console.log('complete')
+                  process.exit()
+              })
+          })
      })
 })
 
