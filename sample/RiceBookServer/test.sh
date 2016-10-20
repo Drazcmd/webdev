84209b0a6f0f1c47217de210f8fb92de6b1624e7
skotep <skotep@gmail.com>
2016-10-20 18:50:38 -0500

    updates

--- sample/RiceBookServer/test.sh
+++ sample/RiceBookServer/test.sh
@@ -4,15 +4,19 @@ PORT=3000
 echo "GET /"
 curl -H 'Content-Type: application/json' http://localhost:${PORT}
 echo ""
+echo ""
 
 echo "GET /articles"
 curl -H 'Content-Type: application/json' http://localhost:${PORT}/articles
+echo ""
 echo ""
 
 echo "POST /article"
-curl -H 'Content-Type: application/json' http://localhost:${PORT}/article -d "{ \"body\":\"This is my new article! $(date)\" }"
+curl -H 'Content-Type: application/json' http://localhost:${PORT}/article -d "{ \"text\":\"This is my new article! $(date)\" }" 
+echo ""
 echo ""
 
 echo "GET /articles"
-curl -H 'Content-Type: application/json' http://localhost:${PORT}/articles
+curl -H 'Content-Type: application/json' http://localhost:${PORT}/articles 
+echo ""
 echo ""
