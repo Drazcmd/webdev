03288c576c939d705fae071212d6064fc341ac98
skotep <skotep@gmail.com>
2016-10-30 18:52:25 -0500

    update for articles

--- sample/dbpost.js
+++ sample/dbpost.js
@@ -1,37 +1,38 @@
-// this is dbpost.js 
-var Post = require('./model.js').Post
-
-exports.setup = function(app) {
-     app.get('/find/:user', find)
-}
+// this is dbarticle.js 
+var Article = require('./model.js').Article
 
 function find(req, res) {
      findByAuthor(req.params.user, function(items) {
-          res.send({items: items})
+          res.send({items})
      })
 }
 
-//////////////////////////////
-// remove these examples 
+module.exports = (app) => {
+     app.get('/find/:user', find)
+}
+
 
 function findByAuthor(author, callback) {
-	Post.find({ author: author }).exec(function(err, items) {
+	Article.find({ author: author }).exec(function(err, items) {
 		console.log('There are ' + items.length + ' entries for ' + author)
 		var totalLength = 0
-		items.forEach(function(post) {
-			totalLength += post.body.length
+		items.forEach(function(article) {
+			totalLength += article.text.length
 		})
 		console.log('average length', totalLength / items.length)		
 		callback(items)
 	})
 }
 
-new Post({ id: 1, author: 'sep1', img: null, date: new Date().getTime(), body: 'This is my first post'}).save()
-new Post({ id: 2, author: 'sep1', img: null, date: new Date().getTime(), body: 'This is my second post'}).save()
-new Post({ id: 3, author: 'jmg3', img: null, date: new Date().getTime(), body: "This is Max's post"}).save(function() {
+//////////////////////////////
+// remove these examples 
+
+new Article({ id: 1, author: 'sep1', img: null, date: new Date().getTime(), text: 'This is my first article'}).save()
+new Article({ id: 2, author: 'sep1', img: null, date: new Date().getTime(), text: 'This is my second article'}).save()
+new Article({ id: 3, author: 'jmg3', img: null, date: new Date().getTime(), text: "This is Max's article"}).save(function() {
      console.log('done with save')
-     Post.find().exec(function(err, items) { 
-          console.log("There are " + items.length + " posts total in db") 
+     Article.find().exec(function(err, items) { 
+          console.log("There are " + items.length + " articles total in db") 
 
           findByAuthor('sep1', function() {
               findByAuthor('jmg3', function() {
@@ -42,4 +43,6 @@ new Post({ id: 3, author: 'jmg3', img: null, date: new Date().getTime(), body: "
      })
 })
 
+//////////////////////////////
+// remove the above example code
 //////////////////////////////
