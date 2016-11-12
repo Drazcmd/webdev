d38536f1158947de22fdcce5879cc927ba4244ea
skotep <skotep@gmail.com>
2016-11-12 17:34:28 -0600

    update

--- sample/uploadCloudinary.js
+++ sample/uploadCloudinary.js
@@ -9,37 +9,47 @@ if (!process.env.CLOUDINARY_URL) {
      process.env.CLOUDINARY_URL="cloudinary:// get value from heroku"
 }
 
-// multer parses multipart form data.  Here we tell
-// it to expect a single file upload named 'image'
-const uploadImage = multer().single('image')
-
-function putImage(req, res) {
-
-	// body-parser provides us the textual formData
-	// which is just title in this case
-	const publicName = req.body.title
+const doUpload = (publicName, req, res, next) => {
 
 	const uploadStream = cloudinary.uploader.upload_stream(result => {    	
-		// create an image tag from the cloudinary upload
-		const image = cloudinary.image(result.public_id, {
-			format: "png", width: 100, height: 130, crop: "fill" 
-		})
-		// create a response to the user's upload
-		res.send(`Uploaded: ${result.url}<br/><a href="${result.url}">${image}</a>`);
-	}, { public_id: publicName })
+         // capture the url and public_id and add to the request
+         req.fileurl = result.url
+         req.fileid = result.public_id
+         next()
+	}, { public_id: req.body[publicName]})
 
 	// multer can save the file locally if we want
-	// instead we do not instruct multer to save the file
-	// and have the file in memory.
-	// multer provides req.file and within is the byte buffer
+	// instead of saving locally, we keep the file in memory
+	// multer provides req.file and within that is the byte buffer
 
 	// we create a passthrough stream to pipe the buffer
-	// to the uploadStream for cloudinary.
+	// to the uploadStream function for cloudinary.
 	const s = new stream.PassThrough()
 	s.end(req.file.buffer)
 	s.pipe(uploadStream)
 	s.on('end', uploadStream.end)
 	// and the end of the buffer we tell cloudinary to end the upload.
+}
+
+// multer parses multipart form data.  Here we tell
+// it to expect a single file upload named 'image'
+// Read this function carefully so you understand
+// what it is doing!
+const uploadImage = (publicName) => (req, res, next) =>
+     multer().single('image')(req, res, () => 
+               doUpload(publicName, req, res, next))
+
+///////////////////////////////////////////////////////////////////////////////
+// These three functions are examples to validate that uploading works
+// You do not want them in your final application
+// 
+function postImage(req, res) {
+   // create an image tag from the cloudinary upload
+   const image = cloudinary.image(req.fileid, {
+       format: "png", width: 100, height: 130, crop: "fill" 
+   })
+   // create a response to the user's upload
+   res.send(`Uploaded: ${req.fileurl}<br/><a href="${req.fileurl}">${image}</a>`);
 }
 
 function getImage(req, res) {
@@ -64,8 +74,18 @@ function setup(app) {
     // but I provide it here for the inclass exercise example
 	app.get('/image', getImage)
 
-	app.post('/image', uploadImage, putImage)
+	// body-parser provides us the textual formData
+	// which is just title in this case
+	app.post('/image', uploadImage('title'), postImage)
 }
+// remove the above three functions and change the last line below to
+//     module.exports = uploadImage
+// 
+// then to use in profile.js do (see comment in getImage about the string 'avatar')
+//     const uploadImage = require('./uploadCloudinary')
+//     app.post('/avatar', uploadImage('avatar'), uploadAvatar)
+//
+///////////////////////////////////////////////////////////////////////////////
 
-module.exports = { putImage, uploadImage, setup }
+module.exports = { uploadImage, setup }
 
